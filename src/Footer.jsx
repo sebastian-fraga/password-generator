0d281d72999fda4f915395efe4d2b5787fbc934d
seba <63276982+sebaa1@users.noreply.github.com>
2026-01-16 19:58:17 -0300

    Update design

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="fixed bottom-0 mt-5 pb-3 w-screen flex justify-center items-center mx-auto">
-            <span className="text-center text-gray-300/50">SEBASTIÁN FRAGA - 2026</span>
+            <span className="text-center font-thin text-gray-300/50">Hecho con ❤️ por <a href="https://github.com/sebastian-fraga" target="_blank" className="font-normal text-gray-50/40 hover:text-red-200/40">Sebastián Fraga</a></span>
         </footer>
     )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,9 @@ import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
     return (
-        <header className="w-full flex justify-center items-center gap-6 py-25 px-5 font-semibold text-center">
+        <header className="w-full flex justify-center items-center gap-6 py-25 px-12 font-semibold text-center">
             <h1 className="text-slate-100 text-3xl">GENERADOR DE CONTRASEÑAS</h1>
-            <FontAwesomeIcon className="text-slate-300" icon={ faLock } />
+            <FontAwesomeIcon className="text-slate-300 text-2xl" icon={ faLock } />
         </header>
     )
 }
